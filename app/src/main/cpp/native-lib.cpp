abfed0e72fa08c30623af4ee49c89b9e11044433
cbd <610131298@qq.com>
2023-06-30 10:20:04 +0800

    交叉编译ffmpeg库开发音视频播放器

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,25 @@
+#include <jni.h>
+#include <string>
+
+// 有坑，会报错，必须混合编译
+/*#include <libavutil/avutil.h>*/
+
+
+extern "C" {
+#include <libavutil/avutil.h>
+}
+
+
+/**
+ * 拿到 ffmpeg 当前版本
+ * @return
+ */
+const char *getFFmpegVer() {
+    return av_version_info();
+}
+
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_example_ffmpegdemo_MainActivity_getFFmpegVersion(JNIEnv *env, jobject thiz) {
+    return env->NewStringUTF(getFFmpegVer());
+}
---
+++ app/src/main/java/com/example/ffmpegdemo/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.ffmpegdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ffmpegdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text =  "当前 FFmpeg 版本为:" + getFFmpegVersion()
+
+    }
+
+
+
+    private external fun getFFmpegVersion(): String?
+
+
+    companion object {
+        // Used to load the 'ffmpegdemo' library on application startup.
+        init {
+            System.loadLibrary("ffmpegdemo")
+        }
+    }
+}
